f0bb35d8cb8fad988fbb48988cc1c89a0767a84c
yapoc <yapspam@yapoc.org>
2018-04-11 16:31:04 +0200

    Passage en modules

---
+++ libs.py
@@ -0,0 +1,25 @@
+import json
+
+def reformat_json (data):
+  try:
+    json_data = json.loads (data)
+  except json.decoder.JSONDecodeError:
+    sys.exit ("Les données fournies n'ont pas l'air d'être du json valide.")
+  return json.dumps (json_data, sort_keys = True, indent = 2)
+
+def save_to_file (json_file, data):
+  try:
+    with open (json_file, "w") as f:
+      f.write (data)
+  except FileNotFoundError:
+    sys.exit ("Le fichier indiqué n'existe pas.")
+
+def load_data_from_file (json_file):
+  try:
+    text = ""
+    with open (json_file, "r") as f:
+      return f.read ()
+  except FileNotFoundError:
+    sys.exit ("Le fichier indiqué n'existe pas.")
+
+
--- pretty_json.py
+++ pretty_json.py
@@ -2,31 +2,7 @@
 import argparse
 import json
 import sys
-
-def load_data_from_file (json_file):
-  try:
-    text = ""
-    with open (json_file, "r") as f:
-      return f.read ()
-  except FileNotFoundError:
-    sys.exit ("Le fichier indiqué n'existe pas.")
-
-def reformat_json (data):
-  try:
-    json_data = json.loads (data)
-  except json.decoder.JSONDecodeError:
-    sys.exit ("Les données fournies n'ont pas l'air d'être du json valide.")
-  return json.dumps (json_data, sort_keys = True, indent = 2)
-
-def save_to_file (json_file, data):
-  try:
-    with open (json_file, "w") as f:
-      f.write (data)
-  except FileNotFoundError:
-    sys.exit ("Le fichier indiqué n'existe pas.")
-
-    
-
+from libs import load_data_from_file, reformat_json, save_to_file
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser ( description = "Joli reformattage de documents json.")
